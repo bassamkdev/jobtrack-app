7418df0142bff413a85e7e66f8046b7c1a530f5e
bassamkdev <bassamkazemi.dev@gmail.com>
2021-01-05 23:58:08 -0800

    connected to backend api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import UnauthenticatedApp from 'unauthenticated-app'
+import {useAuth0} from '@auth0/auth0-react'
+import {FullPageErrorFallback, FullPageSpinner} from 'components/lib'
+
+const AuthenticatedApp = React.lazy(() => import('./authenticated-app'))
 
 function App() {
+  const {isAuthenticated, isLoading, error} = useAuth0()
+  if (isLoading) {
+    return <FullPageSpinner />
+  }
+  if (error) {
+    return <FullPageErrorFallback error={error} />
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Suspense fallback={<FullPageSpinner />}>
+      {isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+    </React.Suspense>
+  )
 }
 
-export default App;
+export default App
