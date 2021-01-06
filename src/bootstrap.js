58a7a8ac2ca78624660c194cbed9136d9f517a03
bassamkdev <bassamkazemi.dev@gmail.com>
2021-01-05 23:56:00 -0800

    bootstrap imports

---
+++ src/bootstrap.js
@@ -0,0 +1,7 @@
+import 'stop-runaway-react-effects/hijack'
+import 'bootstrap/dist/css/bootstrap-reboot.css'
+import '@reach/dialog/styles.css'
+import '@reach/menu-button/styles.css'
+import '@reach/tooltip/styles.css'
+import './styles/global.css'
+import 'react-perfect-scrollbar/dist/css/styles.css'
